840dcaf74c5dbab7db088d3fabb2495249c7f81e
Avirup Ghosh <oviirup@gmail.com>
2025-02-25 16:04:15 +0530

    ♻️ updated theme

--- src/contents/theme/index.ts
+++ src/contents/theme/index.ts
@@ -1,6 +1,11 @@
 import type { PlasmoCSConfig } from 'plasmo';
 import { observe } from 'selector-observer';
-import './styles.scss';
+import './variables.css';
+import './theme.min.css';
+
+export const config: PlasmoCSConfig = {
+  matches: ['https://jiosaavn.com/*', 'https://www.jiosaavn.com/*'],
+};
 
 // remove banners and promotions
 observe('.c-banner', {
@@ -20,15 +25,12 @@ const classNamesMap = {
   'u-color-js-gray': '__JSDX_link_gray',
   'u-color-js-gray-alt-light': '__JSDX_link_gray_alt',
 };
-for (const [source, target] of Object.entries(classNamesMap)) {
-  observe(`.${source}`, {
-    add: (el) => {
+observe('.u-color-js-gray, .u-color-js-gray-alt-light', {
+  add: (el) => {
+    for (const [source, target] of Object.entries(classNamesMap)) {
+      if (!el.classList.contains(source)) continue;
       el.classList.remove(source);
       el.classList.add(target);
-    },
-  });
-}
-
-export const config: PlasmoCSConfig = {
-  matches: ['https://jiosaavn.com/*', 'https://www.jiosaavn.com/*'],
-};
+    }
+  },
+});
