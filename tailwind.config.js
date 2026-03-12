08c461886a7e16c7445fbcfcf9f86c27434264d1
oviirup <49820575+oviirup@users.noreply.github.com>
2026-03-13 01:21:45 +0530

    🗑️ start from scratch

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,ts,tsx}'],
-  theme: {
-    container: { center: true, padding: '24px' },
-    extend: {},
-  },
-}
